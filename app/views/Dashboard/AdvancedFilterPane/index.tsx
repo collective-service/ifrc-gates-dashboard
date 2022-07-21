4e36d623a6dbdc56c8b59329222ad5df04d0a41b
Subina <subina@togglecorp.com>
2022-07-21 11:58:19 +0545

    Add advanced filter layout

--- app/views/Dashboard/AdvancedFilterPane/index.tsx
+++ app/views/Dashboard/AdvancedFilterPane/index.tsx
@@ -3,6 +3,7 @@ import {
     Container,
     Button,
     RadioInput,
+    SelectInput,
 } from '@the-deep/deep-ui';
 
 import styles from './styles.css';
@@ -26,6 +27,77 @@ const filterTypes: FilterTypeEnum[] = [
 const filterTypeKeySelector = (d: FilterTypeEnum) => d.name;
 const filterTypeLabelSelector = (d: FilterTypeEnum) => d.title;
 
+interface ThematicFilterType {
+    id: string;
+    title: string;
+    parent: FilterTypeEnum['name'];
+}
+const thematicFilterItems: ThematicFilterType[] = [
+    {
+        id: '1',
+        title: 'Communication',
+        parent: 'socialBehaviouralIndicators',
+    },
+    {
+        id: '2',
+        title: 'Disease',
+        parent: 'socialBehaviouralIndicators',
+    },
+    {
+        id: '3',
+        title: 'Prevention',
+        parent: 'socialBehaviouralIndicators',
+    },
+    {
+        id: '4',
+        title: 'Health Care',
+        parent: 'socialBehaviouralIndicators',
+    },
+    {
+        id: '5',
+        title: 'Impact Migration',
+        parent: 'socialBehaviouralIndicators',
+    },
+    {
+        id: '6',
+        title: 'Community Engagement',
+        parent: 'socialBehaviouralIndicators',
+    },
+    {
+        id: '7',
+        title: 'General',
+        parent: 'contextualIndicators',
+    },
+    {
+        id: '8',
+        title: 'Epidemiology',
+        parent: 'contextualIndicators',
+    },
+    {
+        id: '9',
+        title: 'Health Services',
+        parent: 'contextualIndicators',
+    },
+    {
+        id: '10',
+        title: 'Other Services',
+        parent: 'contextualIndicators',
+    },
+    {
+        id: '11',
+        title: 'Poltics',
+        parent: 'contextualIndicators',
+    },
+    {
+        id: '12',
+        title: 'Socio-Economic',
+        parent: 'contextualIndicators',
+    },
+];
+
+const thematicKeySelector = (d: ThematicFilterType) => d.id;
+const thematicLabelSelector = (d: ThematicFilterType) => d.title;
+
 function AdvancedFilterPane() {
     const handleApplyFiltersClick = useCallback(() => {
         // eslint-disable-next-line no-console
@@ -33,6 +105,11 @@ function AdvancedFilterPane() {
     }, []);
 
     const [activeFilterType, setActiveFilterType] = useState<string>('contextualIndicators');
+    const [thematicFilterValue, setThematicFilterValue] = useState<string | undefined>();
+
+    const handleChange = useCallback((data: string | undefined) => {
+        setThematicFilterValue(data);
+    }, []);
 
     return (
         <Container
@@ -69,6 +146,17 @@ function AdvancedFilterPane() {
                 value={activeFilterType}
                 onChange={setActiveFilterType}
             />
+            <div className={styles.filterOptions}>
+                <SelectInput
+                    name="thematic"
+                    options={thematicFilterItems}
+                    keySelector={thematicKeySelector}
+                    labelSelector={thematicLabelSelector}
+                    placeholder="Thematic"
+                    value={thematicFilterValue}
+                    onChange={handleChange}
+                />
+            </div>
         </Container>
     );
 }
