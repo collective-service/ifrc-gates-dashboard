c3ed639f209861e531b6f65f729efb06c0059018
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2022-09-30 12:23:31 +0545

    tf configs added;

---
+++ terraform/cdn.tf
@@ -0,0 +1,72 @@
+resource "aws_cloudfront_origin_access_identity" "cloudfront_oia" {
+  comment = "example origin access identify"
+}
+
+resource "aws_cloudfront_distribution" "website_cdn" {
+  enabled = true
+
+  origin {
+    origin_id   = "origin-bucket-${aws_s3_bucket.deploy_bucket.id}"
+    domain_name = aws_s3_bucket.deploy_bucket.bucket_regional_domain_name
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.cloudfront_oia.cloudfront_access_identity_path
+    }
+  }
+
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD", "DELETE", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    min_ttl                = "0"
+    default_ttl            = "300"
+    max_ttl                = "1200"
+    target_origin_id       = "origin-bucket-${aws_s3_bucket.deploy_bucket.id}"
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.acm_certificate.arn
+    ssl_support_method = "sni-only"
+  }
+
+  aliases = [
+    "dashboard.${var.domain_name}"
+  ]
+}
+
+resource "aws_route53_record" "cloudfront_record" {
+    zone_id = data.aws_route53_zone.route53_zone.zone_id
+    type = "A"
+    name = "dashboard.${var.domain_name}"
+    alias {
+      name = aws_cloudfront_distribution.website_cdn.domain_name
+      zone_id = aws_cloudfront_distribution.website_cdn.hosted_zone_id
+      evaluate_target_health = true
+    }
+}
+
+output "website_cdn_id" {
+  value = aws_cloudfront_distribution.website_cdn.id
+}
+
+output "website_endpoint" {
+  value = aws_cloudfront_distribution.website_cdn.domain_name
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable aws_region {
+  default = "eu-west-3"
+}
+
+variable aws_profile {
+  default = "dfs-rcce"
+}
+
+variable dashboard_s3_bucket {
+    default = "test-rcce-website"
+}
+
+variable domain_name {
+  default = "ifrc-gates.dev.datafriendlyspace.org"
+}
